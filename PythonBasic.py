3867c241c33b44c80c586af255c162a4d912996a
BALAJI M <105099034+balajim82@users.noreply.github.com>
2026-03-01 15:47:00 +0530

    Add files via upload
    This File Basic Python Program file.

---
+++ PythonBasic.py
@@ -0,0 +1,11 @@
+print("Hello Python world");
+print("The sum of 10 and 12 is:", 10+12);
+print(10+13);
+a=5;
+if a>10:
+   print("This is true");
+else:
+   print("This is false");
+def add(x,y):
+    return x+y;
+print(add(10,30));
