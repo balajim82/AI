f66c6ccb468faf34691779112aa942d2db21fea6
balajim82 <modadugu.balaji@gmail.com>
2026-03-20 19:40:15 +0530

    developed

--- inventory-mgmt-cli-proj/config.py
+++ inventory-mgmt-cli-proj/config.py
@@ -4,3 +4,7 @@ DB_CONFIG = {
     "host": "localhost",
     "database": "INVENTORY_MGMT_DB",
 }
+
+EXPORT_PATH = "D:/Ai-Programs/GIT_CODE_PYTHON/genai-elite-projects/inventory-mgmt-cli-proj/export_inv"
+
+IMPORT_PATH = "D:/Ai-Programs/GIT_CODE_PYTHON/genai-elite-projects/inventory-mgmt-cli-proj/import_inv"
